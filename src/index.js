5f44b865394082e1907ed4f95c054d0d5b90e3c1
nyamonyamo1031 <79975791+nyamonyamo1031@users.noreply.github.com>
2021-04-23 00:46:33 +0900

    ボタンの削除、完了機能

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import "./styles.scss";
+//テキストボックスの値を入力して、初期化する
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //li生成
+  const li = document.createElement("li");
+  li.className = "list-row";
+
+  //div生成
+  const div = document.createElement("div");
+  div.innerText = inputText;
+
+  //未完了リストに追加
+  document.getElementById("incomplate-list").appendChild(li);
+
+  //完了ボタン生成
+  const complateButton = document.createElement("button");
+  complateButton.innerText = "完了";
+  complateButton.addEventListener("click", () => {
+    alert("完了");
+  });
+  //削除ボタン生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  //liの子要素に各要素を設定
+  li.appendChild(div);
+  div.appendChild(complateButton);
+  div.appendChild(deleteButton);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
