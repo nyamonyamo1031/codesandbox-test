c23c676a0dcd8e2ff640e948f30e4ae8df2145e8
nyamonyamo1031 <79975791+nyamonyamo1031@users.noreply.github.com>
2021-04-23 14:52:20 +0900

    TODOテキストを取得

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const onClickAdd = () => {
   li.className = "list-row";
 
   //div生成
-  const div = document.createElement("div");
-  div.innerText = inputText;
+  const p = document.createElement("p");
+  p.innerText = inputText;
 
   //未完了リストに追加
   document.getElementById("incomplate-list").appendChild(li);
@@ -19,21 +19,31 @@ const onClickAdd = () => {
   const complateButton = document.createElement("button");
   complateButton.innerText = "完了";
   complateButton.addEventListener("click", () => {
-    alert("完了");
+    //完了リストに追加する要素
+    const addTarget = complateButton.parentNode;
+    //TODOテキストを取得
+    const text = addTarget.firstElementChild.innerText;
+
+    //押された完了ボタンの親タグ(li)を未完了リストから削除
+    deleteFromIncomplatelist(complateButton.parentNode);
   });
   //削除ボタン生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    //押された削除ボタンの親タグ(li)を未完了リストから削除
+    deleteFromIncomplatelist(deleteButton.parentNode);
   });
 
   //liの子要素に各要素を設定
-  li.appendChild(div);
-  div.appendChild(complateButton);
-  div.appendChild(deleteButton);
+  li.appendChild(p);
+  li.appendChild(complateButton);
+  li.appendChild(deleteButton);
 };
-
+//未完了リストから指定の要素を削除
+const deleteFromIncomplatelist = (target) => {
+  document.getElementById("incomplate-list").removeChild(target);
+};
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
